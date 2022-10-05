d07cc5d3861c7a0451c60e33bc3ccce14d07e707
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-05 18:19:52 +0300

    React configuration done

---
+++ resources/js/components/MyApp.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function MyApp() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <h2>I am react app</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default MyApp;
+if (document.getElementById("app")) {
+    ReactDOM.render(<MyApp />, document.getElementById("app"));
+}
