6f5691f094c01d3d9277d6eda6b658773d163af6
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-06 20:46:59 +0300

    *Styled login form

--- resources/js/components/MyApp.js
+++ resources/js/components/MyApp.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "../Pages/About";
+// import About from "../Pages/About";
 import Home from "../Pages/Home";
 import Protected from "./Protected";
 function MyApp() {
@@ -12,27 +12,24 @@ function MyApp() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* <Route
                 path="/about"
                 element={
                     <Protected isSignedIn={isSignedIn}>
                         <About />
                     </Protected>
                 }
-            />
+            /> */}
         </Routes>
     );
 }
 export default MyApp;
-// if (document.getElementById("app")) {
-//     ReactDOM.render(
-//         <BrowserRouter>
-//             <MyApp />
-//         </BrowserRouter>,
-//         document.getElementById("app")
-//     );
-// }
+
 import { createRoot } from "react-dom/client";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 const container = document.getElementById("app");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
