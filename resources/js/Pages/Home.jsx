53405d2b5df9e5209bf6ff0a79b703773469bea2
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-06 11:48:17 +0300

    *Done adding router and updating to React 18

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
                 <div className="col-md-8">
                     <h2>Home page</h2>
 
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         Button try
                     </button>
                 </div>
