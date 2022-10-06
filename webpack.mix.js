62c151191c396bdf7e0b49e1dc4e4bf0932ab688
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-06 09:20:04 +0300

    *Added and configured tailwinf

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
 
 /*
  |--------------------------------------------------------------------------
@@ -13,4 +14,9 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .react()
-    .sass("resources/sass/app.scss", "public/css");
+    .sass("resources/sass/app.scss", "public/css")
+    // .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
+    .options({
+        postCss: [tailwindcss("./tailwind.config.js")],
+    })
+    .version();
