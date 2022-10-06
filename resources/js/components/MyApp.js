40581bedd123c9c5eaebcb12282f0b83b08ebebe
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-06 11:47:18 +0300

    *Added configuration for React 18

--- resources/js/components/MyApp.js
+++ resources/js/components/MyApp.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "../Pages/About";
 import Home from "../Pages/Home";
+import Protected from "./Protected";
 function MyApp() {
+    const [isSignedIn, setIsSignedIn] = useState(true);
+    // const signin = () => {
+    //     setIsSignedIn(true);
+    // };
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
+            <Route
+                path="/about"
+                element={
+                    <Protected isSignedIn={isSignedIn}>
+                        <About />
+                    </Protected>
+                }
+            />
         </Routes>
     );
 }
 export default MyApp;
-if (document.getElementById("app")) {
-    ReactDOM.render(
-        <BrowserRouter>
-            <MyApp />
-        </BrowserRouter>,
-        document.getElementById("app")
-    );
-}
+// if (document.getElementById("app")) {
+//     ReactDOM.render(
+//         <BrowserRouter>
+//             <MyApp />
+//         </BrowserRouter>,
+//         document.getElementById("app")
+//     );
+// }
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+    <BrowserRouter>
+        <MyApp />
+    </BrowserRouter>
+);
