80954e4e69d95c6da0943fcf06864c95dc69c36e
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-07 08:58:56 +0300

    *Added event creation action

--- public/js/Store/Actions/EventAction.js
+++ public/js/Store/Actions/EventAction.js
@@ -5,3 +5,26 @@ import {
     EVENT_CREATE_FAIL,
     EVENT_CREATE_RESET,
 } from "../Constants/EventConstants";
+
+export const createEvent = (eventDetails) => async (dispatch, getState) => {
+    dispatch({ type: EVENT_CREATE_REQUEST, payload: event });
+    try {
+        const {
+            userSignin: { userInfo },
+        } = getState();
+        const { data } = await Axios.post("/api/events", event, {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        });
+        dispatch({ type: EVENT_CREATE_SUCCESS, payload: data.event });
+    } catch (error) {
+        dispatch({
+            type: EVENT_CREATE_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
