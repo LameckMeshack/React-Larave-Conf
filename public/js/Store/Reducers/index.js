4341fc803d6d4bc77cbce009a9089e1507f618cc
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-07 13:23:31 +0300

    *Imported reducers

--- public/js/Store/Reducers/index.js
+++ public/js/Store/Reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from "redux";
 import { userSigninReducer } from "./UserReducers";
 import { eventCreateReducer } from "./EventReducers";
+import { departmentCreateReducer } from "./DepartmentReducers";
+import { roleCreateReducer } from "./RoleReducer";
 
 export const rootReducer = combineReducers({
     //user
     userInfo: userSigninReducer,
     //event
     eventCreate: eventCreateReducer,
+    //department
+    departmentCreate: departmentCreateReducer,
+    // role
+    roleCreate: roleCreateReducer,
 });
