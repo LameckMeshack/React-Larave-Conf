fa7c905a2cff51075e5fb19c6bfcac81c2d0b0dc
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-07 09:03:39 +0300

    *Imported event reducer

--- public/js/Store/Reducers/index.js
+++ public/js/Store/Reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { userSigninReducer } from "./UserReducers";
+import { eventCreateReducer } from "./EventReducers";
 
 export const rootReducer = combineReducers({
+    //user
     userInfo: userSigninReducer,
+    //event
+    eventCreate: eventCreateReducer,
 });
