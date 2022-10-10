2c797ce38eb86f6801c474dcc75157a9c0fd5347
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-10 14:01:36 +0300

    *Added category redux files

---
+++ resources/js/Store/Actions/CategoryActions.js
@@ -0,0 +1,60 @@
+export {
+    CATEGORY_CREATE_REQUEST,
+    CATEGORY_CREATE_SUCCESS,
+    CATEGORY_CREATE_FAIL,
+    CATEGORY_LIST_REQUEST,
+    CATEGORY_LIST_SUCCESS,
+    CATEGORY_LIST_FAIL,
+} from "../Constants/CategoryConstants";
+
+export const createCategory = (name) => async (dispatch) => {
+    try {
+        dispatch({
+            type: CATEGORY_CREATE_REQUEST,
+        });
+
+        // const {
+        //     userLogin: { userInfo },
+        // } = getState();
+
+        // const config = {
+        //     headers: {
+        //         userInfo,
+        //         "Content-Type": "application/json",
+        //         Authorization: `Bearer ${userInfo.token}`,
+        //     },
+        // };
+
+        const { data } = await axios.post("/api/categories", (name = { name }));
+
+        dispatch({
+            type: CATEGORY_CREATE_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: CATEGORY_CREATE_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+// // getting all the roles
+export const getCategories = () => async (dispatch) => {
+    dispatch({ type: CATEGORY_LIST_REQUEST });
+    try {
+        const { data } = await axios.get("/api/categories");
+        dispatch({ type: CATEGORY_LIST_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: CATEGORY_LIST_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
---
+++ resources/js/Store/Reducers/CategoryReducers.js
@@ -0,0 +1,35 @@
+export {
+    CATEGORY_CREATE_REQUEST,
+    CATEGORY_CREATE_SUCCESS,
+    CATEGORY_CREATE_FAIL,
+    CATEGORY_LIST_REQUEST,
+    CATEGORY_LIST_SUCCESS,
+    CATEGORY_LIST_FAIL,
+} from "../Constants/CategoryConstants";
+
+export const categoryCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case CATEGORY_CREATE_REQUEST:
+            return { loading: true };
+        case CATEGORY_CREATE_SUCCESS:
+            return { loading: false, category: action.payload };
+        case CATEGORY_CREATE_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+
+export const categoryListReducer = (state= {categories = []}, action) => {
+    switch (action.type) {
+        case CATEGORY_LIST_REQUEST:
+            return { loading: true, categories: [] };
+        case CATEGORY_LIST_SUCCESS:
+            return { loading: false, categories: action.payload };
+        case CATEGORY_LIST_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
