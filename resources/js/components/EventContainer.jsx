e75986d5b90861901078d42984e7c64aaac6b80a
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-10 15:20:23 +0300

    *Added redux event list

--- resources/js/components/EventContainer.jsx
+++ resources/js/components/EventContainer.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import EventCard from "./EventCard";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getEvents } from "../Store/Actions/EventAction";
+import { AuthContext } from "../Context/AuthContext";
 
 function EventContainer() {
     const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getEvents);
-    }, []);
+    const eventList = useSelector((state) => state.eventList);
+    const { loading, error, events } = eventList;
+    // destructure events
+    // const { events: envt } = events;
+    console.log(events);
 
+    const user = useSelector((state) => state.userInfo.userInfo);
+    console.log(user);
+
+    useEffect(() => {
+        dispatch(getEvents());
+    }, []);
+    console.log(events);
     return (
         <div className=" events-container">
+            {/* <EventCard />
             <EventCard />
             <EventCard />
             <EventCard />
             <EventCard />
-            <EventCard />
-            <EventCard />
+            <EventCard /> */}
+            {events && events.length > 0 ? (
+                events.map((event) => <EventCard />)
+            ) : (
+                <h1>No Events</h1>
+            )}
         </div>
     );
 }
