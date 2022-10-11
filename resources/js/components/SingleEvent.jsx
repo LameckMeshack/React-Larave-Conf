7432a26726367956e4378c14f9895c21fccdb520
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-11 11:34:00 +0300

    *refactored sibgle event by adding list of activities

--- resources/js/components/SingleEvent.jsx
+++ resources/js/components/SingleEvent.jsx
@@ -5,7 +5,12 @@ function SingleEvent() {
         <div className="event  bg-green-100">
             <div className="event-top">
                 <div className="event-top-left">
-                    <div className="event-image"></div>
+                    <div className="event-image">
+                        <img
+                            className="event-image"
+                            src="https://picsum.photos/300/200"
+                        />
+                    </div>
                 </div>
                 <div className="event-top-right">
                     <h2>The Grand Event</h2>
@@ -29,69 +34,70 @@ function SingleEvent() {
                 </div>
             </div>
             <div className="event-bottom">
-                <div class="flex flex-col">
-                    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                            <div class="overflow-hidden">
-                                <table class="min-w-full text-center">
-                                    <thead class="border-b">
+                <div className="flex flex-col">
+                    <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                            <div className="overflow-hidden">
+                                <table className="min-w-full text-center">
+                                    <thead className="border-b">
                                         <tr>
                                             <th
                                                 scope="col"
-                                                class="text-sm font-medium text-gray-900 px-6 py-4"
+                                                className="text-sm font-medium text-gray-900 px-6 py-4"
                                             >
                                                 Activity
                                             </th>
                                             <th
                                                 scope="col"
-                                                class="text-sm font-medium text-gray-900 px-6 py-4"
+                                                className="text-sm font-medium text-gray-900 px-6 py-4"
                                             >
                                                 Coordinator
                                             </th>
                                             <th
                                                 scope="col"
-                                                class="text-sm font-medium text-gray-900 px-6 py-4"
+                                                className="text-sm font-medium text-gray-900 px-6 py-4"
                                             >
                                                 Start Date
                                             </th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr class="border-b bg-green-500 border-green-200">
-                                            <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                                        <tr className="border-b bg-green-500 border-green-200">
+                                            <td className="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                                                 Arrangements
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                 Coordinator 1
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                 12/12/2020
                                             </td>
                                         </tr>
-                                        <tr class="border-b bg-orange-500 border-red-200">
-                                            <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                                        <tr className="border-b bg-orange-500 border-red-200">
+                                            <td className="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                                                 Budgetting
                                             </td>
-                                            <td class="text-sm text-gray-500 font-light px-6 py-4 whitespace-nowrap">
+                                            <td className="text-sm text-gray-500 font-light px-6 py-4 whitespace-nowrap">
                                                 Treasurer
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                 12/12/2020
                                             </td>
                                         </tr>
-                                        <tr class="border-b bg-red-500 border-yellow-200">
-                                            <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                                        <tr className="border-b bg-red-500 border-yellow-200">
+                                            <td className="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
                                                 Item Purchasing
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                 Treasurer
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                 12/12/2020
                                             </td>
                                         </tr>
                                     </tbody>
                                 </table>
+                                <button>Add Activity</button>
                             </div>
                         </div>
                     </div>
