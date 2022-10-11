e62721b68b96400c648e1274554b865c8ce9d6a2
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-11 21:47:45 +0300

    *Refactored .Completed register functionality

--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -1,17 +1,71 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
+import { register } from "../Store/Actions/UserAction";
 
 function Register() {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
-    const user = useContext(AuthContext);
-    console.log(user);
+    const roles = useSelector((state) => state.roles);
+    const { loading, error, roles: roleList } = roles;
 
-    // useEffect(() => {
-    //     if (user) {
-    //         navigate("/events");
-    //     } else return;
-    // }, [user]);
+    const departments = useSelector((state) => state.departments);
+    const {
+        loading: deptLoading,
+        error: deptError,
+        departments: deptList,
+    } = departments;
+
+    const [registerData, setRegisterData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+        role_id: "",
+        department_id: "",
+        phone: "",
+        photo: "",
+    });
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        // if register details are empty elert
+
+        if (
+            registerData.name === "" ||
+            registerData.email === "" ||
+            registerData.password === "" ||
+            registerData.password_confirmation === "" ||
+            registerData.role_id === "" ||
+            registerData.department_id === "" ||
+            registerData.phone === "" ||
+            registerData.photo === ""
+        ) {
+            alert("Please fill all the fields");
+        }
+
+        if (registerData.password !== registerData.password_confirmation) {
+            alert("Passwords do not match");
+        } else {
+            const formData = new FormData();
+            formData.append("name", registerData.name);
+            formData.append("email", registerData.email);
+            formData.append("password", registerData.password);
+            formData.append(
+                "password_confirmation",
+                registerData.password_confirmation
+            );
+            formData.append("role_id", registerData.role_id);
+            formData.append("department_id", registerData.department_id);
+            formData.append("phone", registerData.phone);
+            formData.append("photo", registerData.photo);
+
+            dispatch(register(formData));
+            console.log(registerData);
+        }
+    };
+
     return (
         <div className="min-w-screen min-h-screen bg-green-700 flex items-center justify-center px-5 py-5">
             <div
@@ -29,7 +83,7 @@ function Register() {
                                 src="https://www.cytonn.com/assets/img/logos/cytonn_logo.svg"
                             />
                         </div>
-                        <form>
+                        <form onSubmit={handleRegister}>
                             <div className="flex -mx-3">
                                 <div className="w-1/2 px-3 mb-5">
                                     <label
@@ -44,6 +98,14 @@ function Register() {
                                             type="text"
                                             className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                                             placeholder="Liaison"
+                                            name="name"
+                                            value={registerData.name}
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    name: e.target.value,
+                                                })
+                                            }
                                             required
                                         />
                                     </div>
@@ -62,6 +124,14 @@ function Register() {
                                             className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                                             placeholder="ccyton@gmail.com"
                                             required
+                                            name="email"
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    email: e.target.value,
+                                                })
+                                            }
+                                            value={registerData.email}
                                         />
                                     </div>
                                 </div>
@@ -81,12 +151,20 @@ function Register() {
                                             className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                                             placeholder="Liaison"
                                             required
+                                            value={registerData.password}
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    password: e.target.value,
+                                                })
+                                            }
+                                            name="password"
                                         />
                                     </div>
                                 </div>
                                 <div className="w-1/2 px-3 mb-5">
                                     <label
-                                        htmlFor="Email"
+                                        htmlFor=""
                                         className="text-xs font-semibold px-1"
                                     >
                                         Confirm Password
@@ -98,6 +176,16 @@ function Register() {
                                             className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                                             placeholder="confirm password"
                                             required
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    password_confirmation:
+                                                        e.target.value,
+                                                })
+                                            }
+                                            value={
+                                                registerData.password_confirmation
+                                            }
                                         />
                                     </div>
                                 </div>
@@ -111,14 +199,28 @@ function Register() {
                                         Role
                                     </label>
                                     <div className="relative">
-                                        <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                                        <select
+                                            className="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                                            value={registerData.role_id}
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    role_id: e.target.value,
+                                                })
+                                            }
+                                        >
                                             <option className="text-gray-700">
                                                 Select Department
                                             </option>
-
-                                            <option>Department1</option>
-                                            <option>Department2</option>
-                                            <option>Department2</option>
+                                            {roleList &&
+                                                roleList.map((role) => (
+                                                    <option
+                                                        value={role.id}
+                                                        key={role.id}
+                                                    >
+                                                        {role.name}
+                                                    </option>
+                                                ))}
                                         </select>
                                     </div>
                                 </div>
@@ -131,14 +233,29 @@ function Register() {
                                     </label>
                                     {/* select box */}
                                     <div className="relative">
-                                        <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                                        <select
+                                            className="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    department_id:
+                                                        e.target.value,
+                                                })
+                                            }
+                                        >
                                             <option className="text-gray-700">
                                                 Select Department
                                             </option>
 
-                                            <option>Department1</option>
-                                            <option>Department2</option>
-                                            <option>Department2</option>
+                                            {deptList &&
+                                                deptList.map((dept) => (
+                                                    <option
+                                                        value={dept.id}
+                                                        key={dept.id}
+                                                    >
+                                                        {dept.name}
+                                                    </option>
+                                                ))}
                                         </select>
                                     </div>
                                 </div>
@@ -157,6 +274,13 @@ function Register() {
                                             type="tel"
                                             className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                                             placeholder="07@4#$#%$%#$"
+                                            value={registerData.phone}
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    phone: e.target.value,
+                                                })
+                                            }
                                         />
                                     </div>
                                 </div>
@@ -171,7 +295,12 @@ function Register() {
                                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
                                         <input
                                             type="file"
-                                            // className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                                            onChange={(e) =>
+                                                setRegisterData({
+                                                    ...registerData,
+                                                    photo: e.target.files[0],
+                                                })
+                                            }
                                             className="w-full
                                             -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 bg-white border-gray-200 outline-none focus:border-indigo-500"
                                         />
