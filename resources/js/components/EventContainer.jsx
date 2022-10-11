03a92a5b08d34ee3280407af29fca31106643b8c
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-11 11:36:21 +0300

    *Refactored event container by changing the style

--- resources/js/components/EventContainer.jsx
+++ resources/js/components/EventContainer.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getEvents } from "../Store/Actions/EventAction";
 import { AuthContext } from "../Context/AuthContext";
+import { Link } from "react-router-dom";
 
 function EventContainer() {
     const dispatch = useDispatch();
@@ -22,17 +23,33 @@ function EventContainer() {
     console.log(events);
     return (
         <div className=" events-container">
-            {/* <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard /> */}
-            {events && events.length > 0 ? (
-                events.map((event) => <EventCard />)
-            ) : (
-                <h1>No Events</h1>
-            )}
+            <h2>Events</h2>
+            <Link to="/addevent">
+                <button className="btn bg-blue-700 btn-primary create-event-btn">
+                    Create Event
+                </button>
+            </Link>
+
+            <div className="event-cards">
+                {events && events.length > 0 ? (
+                    events.map((event) => (
+                        <Link key={event.id} to={`/events/${event.id}`}>
+                            <EventCard
+                                desc={event.description}
+                                name={event.name}
+                                id={event.id}
+                                // get  category from category table using event.category_id
+                                category={event.category_id}
+                                // get user name from the  user table using event.created_by
+                                created_by={event.created_by}
+                                // created_by={event.created_by}
+                            />
+                        </Link>
+                    ))
+                ) : (
+                    <h1>No Events</h1>
+                )}
+            </div>
         </div>
     );
 }
