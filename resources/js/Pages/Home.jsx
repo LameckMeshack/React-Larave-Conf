e27242126f2953fa36507de4282aff07ea11032e
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-11 14:54:16 +0300

    *Refactored home components

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,17 +1,54 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 function Home() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center mt-3">
-                <div className="col-md-8">
-                    <h2>Home page</h2>
+    const user = useSelector((state) => state.userInfo);
 
-                    <button className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Button try
-                    </button>
+    return (
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+                <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                    <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+                        Login to
+                        <span className="dark:text-violet-400"> create</span>
+                        Events
+                    </h1>
+                    <p className="mt-6 mb-8 text-lg sm:mb-12">
+                        Welcome to our events management system. Please login to
+                        {/* <br className="hidden md:inline lg:hidden"> */}
+                        create and schedule events.
+                        {/* </br> */}
+                    </p>
+                    <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                        {/* if no user is logged in, show the login button */}
+                        {!user ? (
+                            <Link
+                                rel="noopener noreferrer"
+                                to="/login"
+                                className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
+                            >
+                                Login
+                            </Link>
+                        ) : (
+                            <Link
+                                rel="noopener noreferrer"
+                                to="/events"
+                                className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100"
+                            >
+                                View Events
+                            </Link>
+                        )}
+                    </div>
+                </div>
+                <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                    <img
+                        src="assets/svg/Business_SVG.svg"
+                        alt=""
+                        className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+                    />
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 export default Home;
