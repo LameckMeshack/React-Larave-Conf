05422d6131918fa4ac606d20b0ef341bb9750eee
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-11 18:06:49 +0300

    *Refactored blade

--- resources/views/emails/event_reminder.blade.php
+++ resources/views/emails/event_reminder.blade.php
@@ -1,6 +1,8 @@
-<h2>{{$event-name}} is closing in!!!</h2> 
-<br>
+<h2>{{$event->name}} is closing in!!!</h2> 
+
 <p>Hi {{$user->name}},</p>
+<br>
+<p>Hi ,</p>
 <p>Just a reminder that {{$event->name}} is closing in. It will be held on {{$event->start_date}} preparation are to begin todays{{$event->lead_date}}.</p>
     
 Thank you
