3e7dc39007a1c29a97e36f38ebaa28b4b5eb15b7
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-11 11:35:55 +0300

    *Refactored event card

--- resources/js/components/EventCard.jsx
+++ resources/js/components/EventCard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-function EventCard() {
+function EventCard({ id, name, created_by, category, desc }) {
     return (
-        <Link className="card-link" href="#">
+        <div className="card-link">
             <article className="event-card">
                 <img
                     className="event-image"
                     src="https://picsum.photos/300/200"
                 />
                 <div className="article-details">
-                    <h4 className="event-category">category</h4>
-                    <h3 className="event-title">name </h3>
-                    <p className="event-description">desc </p>
-                    <p className="event-author">host Rhino </p>
+                    <h4 className="event-category">{category}</h4>
+                    <h3 className="event-title">{name}</h3>
+                    <p className="event-description">{desc} </p>
+                    <p className="event-author">Organizer: {created_by}</p>
                 </div>
             </article>
-        </Link>
+        </div>
     );
 }
 
