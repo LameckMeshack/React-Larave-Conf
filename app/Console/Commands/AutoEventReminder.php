27a4ad1c96f6e6730bc6c7a8bb9ed48c9061d5d8
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-11 17:50:22 +0300

    *refactored reminder by Adding missing imports

--- app/Console/Commands/AutoEventReminder.php
+++ app/Console/Commands/AutoEventReminder.php
@@ -3,6 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Event;
+use App\Models\User;
+use App\Mail\EventReminder;
+use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
 
 class AutoEventReminder extends Command
 {
@@ -37,13 +42,53 @@ class AutoEventReminder extends Command
      */
     public function handle()
     {
-        $events = Event::where('lead_date', '=', date(''))->get();
+      
+        $events = Event::where('lead_date', '=', today()->format('Y-m-d'))->with('frequency')->get();
+        // dd($events);
         foreach($events as $event){
-            $users = User::where('event_id', '=', $event->id)->get();
+            $users = $event->users;
+    
             foreach($users as $user){
                 Mail::to($user->email)->send(new EventReminder($event));
             }
         }
-      
+         $this->updateLeadDate($events);
+    
+}
+ 
+private function updateLeadDate($events){
+    foreach($events as $event){
+        $frequency = $event->frequency->name;
+         $original_lead_date = Carbon::parse($event->lead_date);
+         $original_start_date = Carbon::parse($event->start_date);
+        if($frequency == 'daily'){
+            $event->lead_date = $original_lead_date->addDays(1);
+            $event->start_date = $original_start_date->addDays(1);
+            $event->save();
+        }
+        if($frequency == 'weekly'){
+            $event->lead_date = $original_lead_date->addWeeks(1);
+            $event->start_date = $original_start_date->addWeeks(1);
+            $event->save();
+        }
+        if($frequency == 'monthly'){
+            $event->lead_date = $original_lead_date->addMonths(1);
+            $event->start_date = $original_start_date->addMonths(1);
+            $event->save();
+        }
+        if($frequency == 'yearly'){
+            $event->lead_date = $original_lead_date->addYears(1);
+            $event->start_date = $original_start_date->addYears(1);
+            $event->save();
+        }
+        // if($frequency == 'custom'){
+        //     $event->lead_date = $event->lead_date->addDays($event->frequency->days);
+        //     $event->start_date = $event->start_date->addDays($event->frequency->days);
+        //     $event->save();
+        // }
     }
+    
+        
+}
+
 }
