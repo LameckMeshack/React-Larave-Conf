c39706ac2ab1f2b4241f42c64bc9f3ad61216586
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-13 22:40:51 +0300

    *Added fillable to the activity table

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -11,11 +11,27 @@ class Activity extends Model
 
     protected $fillable = [
         'name',
+        'incharge',
+        'start_date',
+        'received',
+        'status',
+        'event_id',
     ];
 
-    public function events()
+    // public function events()
+    // {
+    //     return $this->hasMany(Event::class);
+    // }
+
+    //one activity has one event
+    public function event()
     {
-        return $this->hasMany(Event::class);
+        return $this->belongsTo(Event::class);
+    }
+    //one activity has one user
+    public function user()
+    {
+        return $this->belongsTo(User::class,'incharge','id');
     }
 
     
