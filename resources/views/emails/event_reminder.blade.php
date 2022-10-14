0c810e1f04b9eda9dc8bd12d01d6345accc4cca1
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-14 12:04:18 +0300

    *Ractored  blade by adding list

--- resources/views/emails/event_reminder.blade.php
+++ resources/views/emails/event_reminder.blade.php
@@ -1,8 +1,12 @@
+
 <h2>{{$event->name}} is closing in!!!</h2> 
 
 <p>Hi {{$user->name}},</p>
 <br>
-<p>Hi ,</p>
-<p>Just a reminder that {{$event->name}} is closing in. It will be held on {{$event->start_date}} preparation are to begin todays{{$event->lead_date}}.</p>
-    
+<p>Event <span class="bg-blue-400">{{$event->name}} </span>is coming up on {{$event->start_date}} . To prepare for the event, the following actions are required</p>
+
+@foreach($event->activities  as $activity)
+    <p>{{$activity->name ?? ''}} - {{$activity->user ? $activity->user->name : ''}}</p>
+
+    @endforeach
 Thank you
