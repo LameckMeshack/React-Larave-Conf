3d473aa0355bca29415f5fa399ac353d02072e88
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-14 14:49:19 +0300

    *Added a vertical line  and a toggle for department

--- resources/js/components/RolesDepartments.jsx
+++ resources/js/components/RolesDepartments.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getDepartments } from "../Store/Actions/DepartmentAction";
 import { getRoles } from "../Store/Actions/RoleActions";
 
 function RolesDepartments() {
+    const [showDept, setShowDept] = useState(false);
     const dispatch = useDispatch();
     const roles = useSelector((state) => state.roles);
     const departments = useSelector((state) => state.departments);
@@ -16,6 +17,11 @@ function RolesDepartments() {
         departments: departmentList,
     } = departments;
 
+    //TOGGLE SHOW DEPARTMENTS
+    const toggleShowDepartments = () => {
+        setShowDept(!showDept);
+    };
+
     useEffect(() => {
         dispatch(getRoles());
         dispatch(getDepartments());
@@ -25,52 +31,74 @@ function RolesDepartments() {
         <div className="roles flex p-8 justify-center align-center">
             <div className="flex flex-col justify-center mx-8">
                 <h1 className="text-3xl">Roles</h1>
-                <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg text-2xl">
-                        Available Roles
-                    </li>
-                    {roleList &&
-                        roleList.map((role) => (
-                            <li
-                                key={role.id}
-                                className="px-6 py-2 border-b border-gray-200 w-full"
-                            >
-                                {role.name}
-                            </li>
-                        ))}
-                    <li className="px-6 py-2 border-b border-blue-700 w-full rounded-t-lg">
-                        <Link to="/admin/role">
-                            <button className="bg-blue-500 rounded p-2">
-                                Create Role
-                            </button>
-                        </Link>
-                    </li>
-                </ul>
+                {showDept && (
+                    <button
+                        className="bg-blue-500 p-2 rounded"
+                        onClick={toggleShowDepartments}
+                    >
+                        See Roles
+                    </button>
+                )}
+                {!showDept && (
+                    <ul className="bg-white rounded-lg border bg-green-200 border-gray-200 w-96 text-gray-900">
+                        <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg text-2xl">
+                            Available Roles
+                        </li>
+                        {roleList &&
+                            roleList.map((role) => (
+                                <li
+                                    key={role.id}
+                                    className="px-6 py-2 border-b border-gray-200 w-full"
+                                >
+                                    {role.name}
+                                </li>
+                            ))}
+                        <li className="px-6 py-2 border-b border-blue-700 w-full rounded-t-lg">
+                            <Link to="/admin/role">
+                                <button className="bg-blue-500 rounded p-2">
+                                    Create Role
+                                </button>
+                            </Link>
+                        </li>
+                    </ul>
+                )}
             </div>
+
+            <div class="verticalLine"></div>
 
             <div className="flex flex-col justify-center">
                 <h1 className="text-3xl">Departments</h1>
-                <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg text-2xl">
-                        Available Department
-                    </li>
-                    {departmentList &&
-                        departmentList.map((dept) => (
-                            <li
-                                key={dept.id}
-                                className="px-6 py-2 border-b border-gray-200 w-full"
-                            >
-                                {dept.name}
-                            </li>
-                        ))}
-                    <li className="px-6 py-2 border-b border-blue-700 w-full rounded-t-lg">
-                        <Link to="/admin/department">
-                            <button className="bg-blue-500 rounded p-2">
-                                Create Depertment
-                            </button>
-                        </Link>
-                    </li>
-                </ul>
+                {!showDept && (
+                    <button
+                        className="bg-blue-500 p-2 rounded"
+                        onClick={toggleShowDepartments}
+                    >
+                        See Departments
+                    </button>
+                )}
+                {showDept && (
+                    <ul className="bg-white bg-green-200 rounded-lg border border-gray-200 w-96 text-gray-900">
+                        <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg text-2xl">
+                            Available Department
+                        </li>
+                        {departmentList &&
+                            departmentList.map((dept) => (
+                                <li
+                                    key={dept.id}
+                                    className="px-6 py-2 border-b border-gray-200 w-full"
+                                >
+                                    {dept.name}
+                                </li>
+                            ))}
+                        <li className="px-6 py-2 border-b border-blue-700 w-full rounded-t-lg">
+                            <Link to="/admin/department">
+                                <button className="bg-blue-500 rounded p-2">
+                                    Create Depertment
+                                </button>
+                            </Link>
+                        </li>
+                    </ul>
+                )}
             </div>
         </div>
     );
